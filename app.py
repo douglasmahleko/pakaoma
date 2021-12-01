7bf10c5d7a35e473496dc692dd84e84d65549989
Dougy <douglasmahleko@gmail.com>
2021-12-01 21:58:14 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 21:20:03 2021
+
+@author: EliteBook
+"""
+
+import streamlit as st
+
+st.title("zvoda kushanda kutobuda tsinga kushandira mhuri")
+st.subheader("zvinhu azvidi kurara")
